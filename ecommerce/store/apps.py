2968213ce5e7620d7c083a5887a4c78d28c2c8f3
Soluve <pakamo20@gmail.com>
2024-10-14 18:54:16 +0100

    order model updated

--- ecommerce/store/apps.py
+++ ecommerce/store/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+    
+    def ready(self):
+        from django.db.models.signals import post_save
+        from django.contrib.auth.models import User
+        
+        post_save.connect(self.create_customer, sender=User)
+
+    @staticmethod
+    def create_customer(sender, instance, created, **kwargs):
+        from .models import Customer
+        if created:
+            Customer.objects.create(user=instance)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -13,6 +13,9 @@ def store(request):
         if user is not None:
             login(request, user)
             messages.success(request, "You have been logged in")
+            # Ensure the user has a Customer profile
+            if not hasattr(user, 'customer'):
+                Customer.objects.create(user=user)
             return redirect('store')
         else:
             messages.success(request, "There Was An Error Logging In, Please Try Again...")
@@ -31,8 +34,9 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
+        order = {'get_cart_total' : 0, 'get_cart_items' : 0}
         items = []
-    context = {}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
